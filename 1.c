b3e8373240d723e26fc993f3245e9f63fd206de4
xdli_xldi <2538865111@qq.com>
2015-07-31 10:57:21 +0800

    add and commit  1.c

---
+++ 1.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+    > File Name: 1.c
+    > Author: 黎贤东 xdli(peter)
+    > Created Time: 2015年07月31日 星期五 10时56分50秒
+    > Mail:xiandong.li@ingeni.com 
+    > 君集成电路科技有限公司 Ingenic Semiconductor CO.,LTD.
+    > Addr: 9th floor, C3 building, Chuangxing Park, Wangjiang West Road,Gaoxing District, Hefei, China
+    > Http://www.ingenic.cn
+ ************************************************************************/
+
+#include<stdio.h>
+sdafsdfasd
